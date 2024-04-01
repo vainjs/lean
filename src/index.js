b50b7bfa0982d810a7494b8b76487d1f8821736c
youngluo <youngluooo@gmail.com>
2024-04-01 22:11:33 +0800

    feat: init

---
+++ src/index.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+import {Command} from 'commander';
+// import spawn from 'cross-spawn';
+// import fs from 'fs';
+import packageJson  from '../package.json' assert {type: 'json'};
+
+const program = new Command();
+
+program.version(packageJson.version, '-v, --version');
+
+program
+  .command('create <projectName>')
+  .description('create new project')
+  .option('-t, --template <template>', 'vue3,react,npm','npm')
+  .action(function (projectName, template) {
+   console.log(projectName,template);
+  });
+
+
+// program
+//   .command('dev')
+//   .description('')
+//   .action();
+
+// program
+//   .command('build')
+//   .description('')
+//   .option('-f, --format <format>', '需要构建的包方式', 'esm')
+//   .option('-m, --mf')
+//   .action(runBuild);
+
+// program.command('analyse').description('analyse dependencies').action(runAnalyse);
+
+// program.command('lint').description('lint').action(runLint);
+
+program.parse(process.argv);
